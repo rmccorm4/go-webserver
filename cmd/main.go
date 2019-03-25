20f9348a7c69979e64f32d13977f620fde7146d4
Ryan McCormick <rmccorm4@binghamton.edu>
2019-03-25 02:48:07 +0000

    switch to gorilla/mux router

--- cmd/main.go
+++ cmd/main.go
@@ -4,36 +4,30 @@ import (
   "fmt"
   "net/http"
 
-  "github.com/julienschmidt/httprouter"
+  "github.com/gorilla/mux"
 )
 
-// Handler for whenever our webpage receives an HTTP request
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
   // Webpage Output
   w.Header().Set("Content-Type", "text/html")
-  if r.URL.Path == "/" {
-    fmt.Fprint(w, "<h1>Sample Webpage</h1>")
-  } else if r.URL.Path == "/contact" {
-    fmt.Fprint(w, "Contact information placeholder.")
-  } else {
-    w.WriteHeader(http.StatusNotFound)
-    fmt.Fprint(w, "<h1>404 Not Found.</h1>")
-  }
+  fmt.Fprint(w, "<h1>Sample Webpage</h1>")
   // Command-Line Output
   fmt.Printf("%s accessed.\n", r.URL.Path)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func contact(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html")
+  fmt.Fprint(w, "Contact information placeholder.")
 }
 
 func main() {
-  router := httprouter.New()
-  //router.GET("/", rootHandler)
-  router.GET("/hello/:name", Hello)
-  //http.HandleFunc("/", rootHandler)
+  router := mux.NewRouter()
+  router.HandleFunc("/", home)
+  router.HandleFunc("/contact", contact)
+
   url := "localhost:3000"
   fmt.Printf("Web server running on %s\n", url)
+
   // nil instead of router here means to use the default ServeMux
   http.ListenAndServe(url, router)
 }
