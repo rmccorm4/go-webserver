5d5b9aa36bca9cf06ef76ebfb6de4379c72e0481
Ryan McCormick <rmccorm4@binghamton.edu>
2019-03-25 02:07:37 +0000

    add 404 not found page and http status

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,26 @@ import (
   "net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// Handler for whenever our webpage receives an HTTP request
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+  // Webpage Output
   w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Sample Webpage</h1>")
+  if r.URL.Path == "/" {
+    fmt.Fprint(w, "<h1>Sample Webpage</h1>")
+  } else if r.URL.Path == "/contact" {
+    fmt.Fprint(w, "Contact information placeholder.")
+  } else {
+    w.WriteHeader(http.StatusNotFound)
+    fmt.Fprint(w, "<h1>404 Not Found.</h1>")
+  }
+  // Command-Line Output
+  fmt.Printf("%s accessed.\n", r.URL.Path)
 }
 
 func main() {
-  http.HandleFunc("/", handlerFunc)
+  http.HandleFunc("/", rootHandler)
   // nil here means to use the default ServerMux
-  http.ListenAndServe("localhost:3000", nil)
+  url := "localhost:3000"
+  fmt.Printf("Web server running on %s\n", url)
+  http.ListenAndServe(url, nil)
 }
